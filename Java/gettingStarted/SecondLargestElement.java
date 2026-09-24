c08cb85532be405bee61a7b48f89b2f04e5a423d
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-09-24 23:37:37 +0530

    Array Problems

--- Java/gettingStarted/SecondLargestElement.java
+++ Java/gettingStarted/SecondLargestElement.java
@@ -23,9 +23,6 @@ public class SecondLargestElement {
     return secondLargest;
    
 }
-
-
-
     public static void main(String[] args) {
         int [] arr={1,2,3,4,5,6};
         System.out.println(Arrays.toString(arr));
