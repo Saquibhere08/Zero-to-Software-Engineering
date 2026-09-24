c4c0380dde34158cd335fa8d92d879ad57e345f1
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-09-24 23:36:45 +0530

    Array Problem

---
+++ Java/gettingStarted/SecondLargestElement.java
@@ -0,0 +1,35 @@
+package Java.gettingStarted;
+
+import java.util.Arrays;
+
+public class SecondLargestElement {
+   
+    public static  int secondLargestElement(int[] nums) {
+        int largest=Integer.MIN_VALUE;
+        int secondLargest=Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+           
+            if(nums[i]>largest){
+                secondLargest=largest;
+                largest=nums[i];                
+            }
+            else if (nums[i] > secondLargest && nums[i] != largest) {
+        secondLargest = nums[i];
+    }    
+    }
+     if(secondLargest==Integer.MIN_VALUE){
+        return -1;
+    }
+    return secondLargest;
+   
+}
+
+
+
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5,6};
+        System.out.println(Arrays.toString(arr));
+        int res=secondLargestElement(arr);
+        System.out.println("Second largest Element: "+res);
+    }
+}
